4d266328dced1a7be2e64a0afcc39ad35f5b4130
Sergii Puzyrov <sergii.puzyrov@gmail.com>
2018-03-28 13:54:46 +0300

    Remove unused imports

--- src/ua/edu/chmnu/net/udp/multicast/MultiCastSender.java
+++ src/ua/edu/chmnu/net/udp/multicast/MultiCastSender.java
@@ -2,10 +2,8 @@ package ua.edu.chmnu.net.udp.multicast;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
-import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
