26141b9a8157ad22c03d3cca80f5066f492ae015
Sergii <sergii5591@gmail.com>
2026-04-09 02:14:17 +0300

    Fixed equals for mouse message event

--- src/main/java/ua/edu/chmnu/ki/networks/mouse/model/MousePositionEvent.java
+++ src/main/java/ua/edu/chmnu/ki/networks/mouse/model/MousePositionEvent.java
@@ -4,6 +4,7 @@ import ua.edu.chmnu.ki.networks.common.DesktopBoundsUtils;
 
 import java.awt.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 public record MousePositionEvent(double x, double y, ScreenBounds screenBounds, long timestamp) implements Serializable {
     public MousePositionEvent(Point point, Rectangle rectangle, long timestamp) {
@@ -13,5 +14,17 @@ public record MousePositionEvent(double x, double y, ScreenBounds screenBounds, 
                 new ScreenBounds(rectangle),
                 timestamp
         );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        MousePositionEvent that = (MousePositionEvent) o;
+        return Double.compare(x, that.x) == 0 && Double.compare(y, that.y) == 0 && Objects.equals(screenBounds, that.screenBounds);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, screenBounds);
     }
 }
